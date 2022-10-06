b4977bfe29375c6add073942e0b2a5a484d69d9d
echoriiku <ricardo.ars@pm.me>
2022-10-06 02:37:22 -0300

    crated a script to make the symlinks

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -sf $HOME/myrepos/dotfiles/alacritty/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/bottom/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/fish/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/helix/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/hypr/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/kitty/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/lazygit/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/mpv/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/tmux/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/xplr/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/zellij/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/starship.toml ~/.config/
