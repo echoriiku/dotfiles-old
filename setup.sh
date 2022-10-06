74e2999c57bc8e40cf59cbb848a32684a11f2c65
echoriiku <ricardo.ars@pm.me>
2022-10-06 03:37:14 -0300

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ ln -sf $HOME/myrepos/dotfiles/fish/ ~/.config/
 ln -sf $HOME/myrepos/dotfiles/helix/ ~/.config/
 ln -sf $HOME/myrepos/dotfiles/hypr/ ~/.config/
 ln -sf $HOME/myrepos/dotfiles/kitty/ ~/.config/
-ln -sf $HOME/myrepos/dotfiles/lazygit/ ~/.config/
+ln -sf $HOME/myrepos/dotfiles/gitui/ ~/.config/
 ln -sf $HOME/myrepos/dotfiles/mpv/ ~/.config/
 ln -sf $HOME/myrepos/dotfiles/tmux/ ~/.config/
 ln -sf $HOME/myrepos/dotfiles/xplr/ ~/.config/
