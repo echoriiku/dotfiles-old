52b83eac00b1e945a31669f98a55a2cf329fbba1
echoriiku <ricardo.ars@pm.me>
2022-10-10 03:45:48 -0300

    "fixed" again the setup script"

--- setup.sh
+++ setup.sh
@@ -7,19 +7,19 @@ sleep 2
 mkdir -pv $HOME/.config/{alacritty,bottom,btop,fish,helix,hypr,kitty,gitui,mpv,tmux,rofi,xplr,zellij}
 
 # symlink the config files
-ln -sfv $HOME/myrepos/dotfiles/alacritty/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/bottom/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/btop/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/fish/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/helix/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/hypr/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/kitty/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/gitui/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/mpv/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/tmux/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/rofi $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/xplr/ $HOME/.config/
-ln -sfv $HOME/myrepos/dotfiles/zellij/ $HOME/.config/
+ln -sfv $HOME/myrepos/dotfiles/alacritty/* $HOME/.config/alacritty
+ln -sfv $HOME/myrepos/dotfiles/bottom/* $HOME/.config/bottom
+ln -sfv $HOME/myrepos/dotfiles/btop/* $HOME/.config/btop
+ln -sfv $HOME/myrepos/dotfiles/fish/* $HOME/.config/fish
+ln -sfv $HOME/myrepos/dotfiles/helix/* $HOME/.config/helix
+ln -sfv $HOME/myrepos/dotfiles/hypr/* $HOME/.config/hypr
+ln -sfv $HOME/myrepos/dotfiles/kitty/* $HOME/.config/kitty
+ln -sfv $HOME/myrepos/dotfiles/gitui/* $HOME/.config/gitui
+ln -sfv $HOME/myrepos/dotfiles/mpv/* $HOME/.config/mpv
+ln -sfv $HOME/myrepos/dotfiles/tmux/* $HOME/.config/tmux
+ln -sfv $HOME/myrepos/dotfiles/rofi/* $HOME/.config/rofi
+ln -sfv $HOME/myrepos/dotfiles/xplr/* $HOME/.config/xplr
+ln -sfv $HOME/myrepos/dotfiles/zellij/* $HOME/.config/zellij
 ln -sfv $HOME/myrepos/dotfiles/starship.toml $HOME/.config/
 ln -sfv $HOME/myrepos/dotfiles/.vimrc $HOME 
 
